55813602425587e5fbdc11a8e253126495ef9812
Rishav Baral <rishavbaral6@gmail.com>
2019-10-15 16:15:03 -0700

    add debugger and ip logs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,11 +8,12 @@ class ApplicationController < ActionController::Base
     protected
       
     def block_ip_addresses
-          head :unauthorized if current_ip_address == "162.156.172.219"
+        logger.debug "your ip address is #{current_ip_address}"
+        head :unauthorized if current_ip_address == "162.156.172.219"
     end
       
     def current_ip_address
-          request.env['HTTP_X_REAL_IP'] || request.env['REMOTE_ADDR']
+        request.env['HTTP_X_REAL_IP'] || request.env['REMOTE_ADDR']
     end
 
 end
