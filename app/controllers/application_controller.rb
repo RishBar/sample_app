6216590df2a6b364c903d3f44d4bcd23cad689c3
Rishav Baral <rishavbaral6@gmail.com>
2019-10-15 15:49:10 -0700

    block duls ip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
     def yo_dul
         render html: "yo dul!"
     end
+
+    before_action :block_ip_addresses
+      
+    protected
+      
+    def block_ip_addresses
+          head :unauthorized if current_ip_address == "162.156.172.219"
+    end
+      
+    def current_ip_address
+          request.env['HTTP_X_REAL_IP'] || request.env['REMOTE_ADDR']
+    end
+
 end
